9efafa99659dc3c3c2af614cd7d0fc7d679b369c
Jamie <31694@ma-web.nl>
2022-10-05 09:43:11 +0200

    ComponentDidMount and ComponentDIdUpdate

---
+++ src/Data/tasks.js
@@ -0,0 +1,33 @@
+tasks = [
+    {
+        name: "Eten",
+        done: false,
+        id: 1
+    },
+    {
+        name: "Drinken",
+        done: false,
+        id: 2
+    },
+    {
+        name: "Kat uitlaten",
+        done: false,
+        id: 3
+    },
+    {
+        name: "Werk",
+        done: false,
+        id: 4
+    },
+    {
+        name: "Ademen",
+        done: false,
+        id: 5
+    },
+    {
+        name: "Praten",
+        done: false,
+        id: 6
+    },
+    
+];
--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -1,56 +1,46 @@
+import React from "react";
 import Item from "../Item/Item";
 import "./todo.css";
 
-const Todo = (props) => {
+class Todo extends React.Component {
   
-    const tasks = [
-        {
-            name: "Eten",
-            done: false,
-            id: 1
-        },
-        {
-            name: "Drinken",
-            done: false,
-            id: 2
-        },
-        {
-            name: "Kat uitlaten",
-            done: false,
-            id: 3
-        },
-        {
-            name: "Werk",
-            done: false,
-            id: 4
-        },
-        {
-            name: "Ademen",
-            done: false,
-            id: 5
-        },
-        {
-            name: "Praten",
-            done: false,
-            id: 6
-        },
-        
-    ];
-
-    let listItems = tasks.map ( task => {
+    constructor(props){
+        super(props);
+        this.state = {
+            tasks: [],
+        }
+    }
+    
+    /*
+    listItems = this.state.tasks.map ( task => {
        return <Item key={task.id} name={task.name} done={task.done}/>
-    });
+    }); */
 
-    return (
-        <article className="todo">
-            <header className="todo__header">
-                <h1 className="todo__heading">Things to do:</h1>
-            </header>
-            <ul className="todo__list">
-                {listItems}
-            </ul>
-        </article>
-    );
+    // ComponentDidMount runt na het aanmaken en tekenen van een component
+    componentDidMount() {
+        this.setState ({
+            tasks: this.state.tasks.push(2),
+        });
+    }
+
+    // ComponentDidUpdate runt na het updaten van de state
+    componentDidUpdate() {
+
+    }
+
+    render(){
+        console.log("Vanuit de render");
+        return (
+            <article className="todo">
+                <header className="todo__header">
+                    <h1 className="todo__heading">Things to do:</h1>
+                </header>
+                <ul className="todo__list">
+                    {this.listItems}
+                </ul>
+            </article>
+        );
+    }
 }
 
 export default Todo;
